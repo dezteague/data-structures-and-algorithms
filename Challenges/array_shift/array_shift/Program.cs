360a2e9d481d7ac95cdcf1e0aabf16f3a5abc4b1
Deziree' Teague <dez.teague@gmail.com>
2019-01-09 11:40:06 -0800

    array shift method is working

--- Challenges/array_shift/array_shift/Program.cs
+++ Challenges/array_shift/array_shift/Program.cs
@@ -6,11 +6,40 @@ namespace array_shift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            
+            int[] inputArray = new int[] { 2, 4, 6, 8 };
+            int addValue = 5;
+            int[] outputArray = ArrayShift(inputArray, addValue);
+            for (int i = 0; i < outputArray.Length; i++)
+            {
+                Console.WriteLine(outputArray[i]);
+            }
         }
-
-        static void ArrayShift()
+  
+        static int[] ArrayShift(int[] inputArray, int addValue)
         {
+            //set length of the new array
+            int[] outputArray = new int[inputArray.Length + 1];
+
+            //loop through the first half of the array
+            for (int i = 0; i < inputArray.Length / 2; i++)
+            {
+                //assign indeces from input to output
+                outputArray[i] = inputArray[i];
+            }
+
+            //insert the additional value after the midpoint
+            outputArray[inputArray.Length / 2] = addValue ;
+
+            //loop through the second half of the array
+            for (int i = inputArray.Length/2 + 1; i < outputArray.Length; i++)
+            {
+                //assign indeces from input to output, after the midpoint
+                outputArray[i] = inputArray[i-1];
+            }
+
+            return outputArray;
 
         }
     }
