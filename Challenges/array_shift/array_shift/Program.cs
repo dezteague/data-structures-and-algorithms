bd1bf4f7a62798231a1b46f6cf4d93d9e3dd5c9e
Deziree' Teague <dez.teague@gmail.com>
2019-01-09 11:41:00 -0800

    tested with longer array

--- Challenges/array_shift/array_shift/Program.cs
+++ Challenges/array_shift/array_shift/Program.cs
@@ -5,11 +5,9 @@ namespace array_shift
     class Program
     {
         static void Main(string[] args)
-        {
-           
-            
-            int[] inputArray = new int[] { 2, 4, 6, 8 };
-            int addValue = 5;
+        { 
+            int[] inputArray = new int[] { 2, 4, 6, 8, 10, 11 };
+            int addValue = 25;
             int[] outputArray = ArrayShift(inputArray, addValue);
             for (int i = 0; i < outputArray.Length; i++)
             {
