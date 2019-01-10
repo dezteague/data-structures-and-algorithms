f7852625f20652b991655d0bcd7abf09df5cfef7
Deziree' Teague <dez.teague@gmail.com>
2019-01-09 19:47:24 -0800

    set up Binary Search method

--- Challenges/array_binary_search/array_binary_search/Program.cs
+++ Challenges/array_binary_search/array_binary_search/Program.cs
@@ -6,7 +6,34 @@ namespace array_binary_search
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] binaryArray = new int[] { 4, 8, 15, 16, 23, 42 };
+    
+            int integer = 15;
+
+            //int indexOfSortedArray = Method(parameters)
+        }
+
+        static int BinarySearch(int[] binaryArray, int integer)
+        {
+            int midpoint = binaryArray.Length/2;
+
+            if (integer == binaryArray[midpoint])
+            {
+                return midpoint;
+            }
+
+            int arrayStart = binaryArray[0];
+            //int arrayFinish = binaryArray.Length -1;
+
+            if (integer < binaryArray[midpoint])
+            {
+                //return BinarySearch (binaryArray(from 0 to midpoint), integer)
+            }
+            else
+            {
+                //int offsetIndex = BinarySearch (binaryArray(from midpoint to end), integer)
+                if 
+            }
         }
     }
 }
