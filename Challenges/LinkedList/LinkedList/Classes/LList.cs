55e72deab4cd4ce4008038168c6b2af95e1f172e
Deziree' Teague <dez.teague@gmail.com>
2019-01-14 09:52:02 -0800

    finished Includes method

--- Challenges/LinkedList/LinkedList/Classes/LList.cs
+++ Challenges/LinkedList/LinkedList/Classes/LList.cs
@@ -6,7 +6,9 @@ namespace LinkedList.Classes
 {
     class LList
     {
+        //properties of Linked List Class: Head, Current
         public Node Head { get; set; }
+        public Node Current { get; set; }
         
         //to add a new value
         public void Insert(int value)
@@ -17,6 +19,32 @@ namespace LinkedList.Classes
             node.Next = Head;
             //then move the Head to point to the new Node
             Head = node;
+        }
+
+        //to traverse through the data structure and see if a value exists. pass in a value to check
+        //method will return true or false
+        public bool Includes(int value)
+        {
+            //traversal starts at the beginning of the list, using the current node
+            Current = Head;
+            //!= verifies that we are not at the end of the list
+            while(Current.Next != null)
+            {
+                //conditional will return true if you've found the value
+                if(Current.Value == value)
+                {
+                    return true;
+                }
+                //move on to the Next value in the list
+                Current = Current.Next;
+            }
+            //repeats condition applied in the while loop, just to check the very last node
+            if (Current.Value == value)
+            {
+                return true;
+            }
+            //bool default
+            return false;
         }
     }
 }
