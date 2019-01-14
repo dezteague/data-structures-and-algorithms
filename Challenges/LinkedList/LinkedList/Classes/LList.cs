132dbc156829c2cace135c98bec31cc0ae5898a6
Deziree' Teague <dez.teague@gmail.com>
2019-01-14 09:40:30 -0800

    set up Node Class with properties and constructor

---
+++ Challenges/LinkedList/LinkedList/Classes/LList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Classes
+{
+    class LList
+    {
+        public Node Head { get; set; }
+        
+        //to add a new value
+        public void Insert(int value)
+        {
+            //create a new node 
+            Node node = new Node(value);
+
+        }
+    }
+}
