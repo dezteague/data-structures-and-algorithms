046bf5044422a8e75cc4e1817e049d9e4e6e7edb
Deziree' Teague <dez.teague@gmail.com>
2019-01-15 10:32:16 -0800

    finished insertBefore and insertAfter test

--- Data-Structures/LinkedList/LinkedListTest/UnitTest1.cs
+++ Data-Structures/LinkedList/LinkedListTest/UnitTest1.cs
@@ -46,26 +46,35 @@ namespace LinkedListTest
 
         [Fact]
         public void AppendValueTest()
-        {
-            
+        { 
             LList test = new LList();
             test.Insert(34);
-            test.Insert(56);
             test.Append(66);
             bool Booltest = test.Includes(66);
             Assert.Equal(66, test.Head.Next.Value);
         }
 
         [Fact]
-        public void InsertValueBeforeTest()
+        public void InsertBeforeTest()
         {
-            
+            LList test = new LList();
+            test.Insert(13);
+            test.Insert(11);
+            test.InsertBefore(13, 12);
+            bool Booltest = test.Includes(12);
+            Assert.Equal(12, test.Head.Next.Value);
         }
 
         [Fact]
-        public void InsertValueAfterTest()
+        public void InserAfterTest()
         {
-            
+            LList test = new LList();
+            test.Insert(13);
+            test.Insert(11);
+            test.InsertAfter(11, 12);
+            bool Booltest = test.Includes(12);
+            Assert.Equal(12, test.Head.Next.Value);
         }
+
     }
 }
