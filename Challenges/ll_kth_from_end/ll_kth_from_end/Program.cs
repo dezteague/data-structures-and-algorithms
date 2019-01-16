12d9ba02f309b90fcdcfde9c3993abfa4c34cfe4
Deziree' Teague <dez.teague@gmail.com>
2019-01-16 11:17:24 -0800

    wrote logic for kth method

--- Challenges/ll_kth_from_end/ll_kth_from_end/Program.cs
+++ Challenges/ll_kth_from_end/ll_kth_from_end/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LinkedList.Classes;
 
 namespace ll_kth_from_end
 {
@@ -7,6 +8,47 @@ namespace ll_kth_from_end
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            //instantiate linked list and inserted values
+            LList list = new LList();
+            list.Insert(4);
+            list.Insert(8);
+            list.Insert(15);
+            list.Insert(23);
+            list.Insert(31);
+            list.Insert(44);
+            list.Print();
+            //invoke the method using list and k as parameters
+            Console.WriteLine($"{FindKthFromEnd(list, 0)}");
+
+            Console.ReadLine();
+        }
+
+        public static int FindKthFromEnd(LList list, int k)
+        {
+            list.Current = list.Head;
+            Node Traversal = list.Head;
+            int counter = 0; 
+            while (list.Current.Next != null)
+            {
+                counter++;
+                if( counter > k)
+                {
+                    Traversal = Traversal.Next;
+                }
+                list.Current = list.Current.Next;
+            }
+            if (k > counter)
+            {
+
+                Console.WriteLine("Sorry");
+                return 0;
+            }
+            else
+            {
+
+            }
+            return Traversal.Value;
         }
     }
 }
