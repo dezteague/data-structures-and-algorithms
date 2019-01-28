0d41b9ecec0b5a8fba433e5f09a28df810d9348d
Deziree' Teague <dez.teague@gmail.com>
2019-01-28 09:54:56 -0800

    created node class with properties and constructor

---
+++ Data-Structures/Tree/Tree/Classes/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    class Node
+    {
+        public int Value { get; set; }
+        public int LeftChild { get; set; }
+        public int RightChild { get; set; }
+
+        public Node()
+        {
+
+        }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
