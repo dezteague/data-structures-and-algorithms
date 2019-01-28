c6f37c86de85087d4e113254b74b454776a7702a
Deziree' Teague <dez.teague@gmail.com>
2019-01-27 22:49:50 -0800

    added last if statement to validate empty stack

--- Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -3,11 +3,13 @@ using StacksAndQueues.Classes;
 
 namespace MultiBracketValidation
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string teststring = "({})";
+            MultiBracketValidation(teststring);
         }
 
         public static bool MultiBracketValidation(string input)
@@ -18,7 +20,7 @@ namespace MultiBracketValidation
             //loop through the string of input
             for (int i = 0; i < input.Length; i++)
             {
-                 //if the string has opening brackets, push them into the stack
+                //if the string has opening brackets, push them into the stack
                 if (input[i] == '(' || input[i] == '[' || input[i] == '{')
                 {
                     openingBracketStack.Push(input[i]);
@@ -27,7 +29,7 @@ namespace MultiBracketValidation
                 //if there is a matching closing bracket, pop the opening bracket out of the stack
                 if (input[i] == ')')
                 {
-                    if(openingBracketStack.Top !=null && openingBracketStack.Top.Value == '(')
+                    if (openingBracketStack.Top != null && openingBracketStack.Top.Value == '(')
                     {
                         openingBracketStack.Pop();
                     }
@@ -62,7 +64,17 @@ namespace MultiBracketValidation
                         return false;
                     }
                 }
-            } 
+            }
+                //if the stack is empty, all the brackets have been matched
+                if (openingBracketStack == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+             
         }
     }
 }
