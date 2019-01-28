9405dc8d7679b953d5824a15aa4d997a359d168f
Deziree' Teague <dez.teague@gmail.com>
2019-01-27 22:35:18 -0800

    instaniated new stack, added for loop to push in opening brackets

--- Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using StacksAndQueues.Classes;
 
 namespace MultiBracketValidation
 {
@@ -7,6 +8,35 @@ namespace MultiBracketValidation
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static bool MultiBracketValidation(string input)
+        {
+            //instantiate a stack to hold the opening brackets
+            Stack openingBracketStack = new Stack();
+
+            //loop through the string of input
+            for (int i = 0; i < input.Length; i++)
+            {
+                 //if the string has opening brackets, push them into the stack
+                if (input[i] == '(' || input[i] == '[' || input[i] == '{')
+                {
+                    openingBracketStack.Push(input[i]);
+                }
+
+                //if there is a matching closing bracket, pop the opening bracket out of the stack
+                if (input[i] == ')')
+                {
+                    if(openingBracketStack.Top !=null && openingBracketStack.Top.Value == '[')
+                    {
+                        openingBracketStack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            } 
         }
     }
 }
