102ca328886ce627f3ba3e68d403c57a74f42a4f
Deziree' Teague <dez.teague@gmail.com>
2019-01-28 11:48:37 -0800

    successful tests for binary tree

--- Data-Structures/Tree/TreeTest/UnitTest1.cs
+++ Data-Structures/Tree/TreeTest/UnitTest1.cs
@@ -1,14 +1,49 @@
 using System;
 using Xunit;
+using Tree;
+using Tree.Classes;
 
 namespace TreeTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void PreOrderTest()
         {
+            Node node = new Node(10);
+            BinaryTree tree = new BinaryTree(node);
+            node.LeftChild = new Node(20);
+            node.RightChild = new Node(30);
 
+            int[] preordertest = tree.PreOrder(node);
+
+            Assert.Equal(10, preordertest[0]);
+        }
+
+        [Fact]
+        public void InOrderTest()
+        {
+            Node node = new Node(10);
+            BinaryTree tree = new BinaryTree(node);
+            node.LeftChild = new Node(20);
+            node.RightChild = new Node(30);
+
+            int[] inordertest = tree.InOrder(node);
+
+            Assert.Equal(10, inordertest[1]);
+        }
+
+        [Fact]
+        public void PostOrderTest()
+        {
+            Node node = new Node(10);
+            BinaryTree tree = new BinaryTree(node);
+            node.LeftChild = new Node(20);
+            node.RightChild = new Node(30);
+
+            int[] postordertest = tree.PostOrder(node);
+
+            Assert.Equal(10, postordertest[2]);
         }
     }
 }
