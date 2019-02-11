86cce97cfc4c02ec2e4d0e57cd3a5eeccd63da96
Deziree' Teague <dez.teague@gmail.com>
2019-02-11 14:51:59 -0800

    set up BinaryTreeHeight files

--- Challenges/AreCharactersUnique/UniqueCharsTest/UnitTest1.cs
+++ Challenges/AreCharactersUnique/UniqueCharsTest/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace UniqueCharsTest
         public void DuplicatesReturnFalseTest()
         {
             string numbers = "2263321";
+            //duplicates return false
             Assert.False(Program.AreCharactersUnique(numbers));
         }
 
@@ -17,6 +18,7 @@ namespace UniqueCharsTest
         public void UniqueCharsReturnTrueTest()
         {
             string letters = "abcdefg";
+            //unique characters return true
             Assert.True(Program.AreCharactersUnique(letters));
         }
 
@@ -24,6 +26,7 @@ namespace UniqueCharsTest
         public void CaseSensitiveTest()
         {
             string cases = "aAbBcC";
+            //lower case and upper case versions of the same letter are considered unique
             Assert.True(Program.AreCharactersUnique(cases));
         }
     }
---
+++ Challenges/CalculateBinaryTreeHeight/BinaryTreeHeightTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BinaryTreeHeightTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
