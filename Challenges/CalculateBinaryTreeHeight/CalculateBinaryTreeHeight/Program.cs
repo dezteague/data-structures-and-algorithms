f8fe6602232e0aaf8d59b8ba2faa6f1f9f2c6248
Deziree' Teague <dez.teague@gmail.com>
2019-02-11 14:56:08 -0800

    instantiated binary tree in console

--- Challenges/CalculateBinaryTreeHeight/CalculateBinaryTreeHeight/Program.cs
+++ Challenges/CalculateBinaryTreeHeight/CalculateBinaryTreeHeight/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tree.Classes;
 
 namespace CalculateBinaryTreeHeight
 {
@@ -7,6 +8,11 @@ namespace CalculateBinaryTreeHeight
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Node node = new Node(10);
+            node.LeftChild = new Node(20);
+            node.RightChild = new Node(30);
+            node.RightChild.LeftChild = new Node(40);
+            node.RightChild.RightChild = new Node(50);
         }
     }
 }
