2e7d645b3378c41b4c5aa9a9a370d962d8b9e4a2
Deziree' Teague <dez.teague@gmail.com>
2019-02-11 15:00:32 -0800

    set if else statement for max height

--- Challenges/CalculateBinaryTreeHeight/CalculateBinaryTreeHeight/Program.cs
+++ Challenges/CalculateBinaryTreeHeight/CalculateBinaryTreeHeight/Program.cs
@@ -3,16 +3,28 @@ using Tree.Classes;
 
 namespace CalculateBinaryTreeHeight
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Node node = new Node(10);
-            node.LeftChild = new Node(20);
-            node.RightChild = new Node(30);
-            node.RightChild.LeftChild = new Node(40);
-            node.RightChild.RightChild = new Node(50);
+            Node root = new Node(10);
+            root.LeftChild = new Node(20);
+            root.RightChild = new Node(30);
+            root.RightChild.LeftChild = new Node(40);
+            root.RightChild.RightChild = new Node(50);
+        }
+
+        public static int CalculateBinaryTreeHeight(Node root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return Math.Max(CalculateBinaryTreeHeight(root.LeftChild), CalculateBinaryTreeHeight(root.RightChild));
+            }
         }
     }
 }
