d9a3e3f68c3754312fe76df5659c9211bef76870
Deziree' Teague <dez.teague@gmail.com>
2019-02-11 16:41:37 -0800

    returns height tests work

--- Challenges/CalculateBinaryTreeHeight/BinaryTreeHeightTest/UnitTest1.cs
+++ Challenges/CalculateBinaryTreeHeight/BinaryTreeHeightTest/UnitTest1.cs
@@ -18,5 +18,35 @@ namespace BinaryTreeHeightTest
 
             Assert.Equal(2, height);
         }
+
+        [Fact]
+        public void ReturnsHeightTestTwo()
+        {
+            Node root = new Node(10);
+            root.LeftChild = new Node(20);
+            root.RightChild = new Node(30);
+            root.RightChild.LeftChild = new Node(40);
+            root.RightChild.RightChild = new Node(50);
+
+            int height = Program.CalculateBinaryTreeHeight(root);
+
+            Assert.Equal(3, height);
+        }
+
+        [Fact]
+        public void ReturnsHeightTestThree()
+        {
+            Node root = new Node(10);
+            root.LeftChild = new Node(20);
+            root.RightChild = new Node(30);
+            root.RightChild.LeftChild = new Node(40);
+            root.RightChild.RightChild = new Node(50);
+            root.RightChild.LeftChild.LeftChild = new Node(60);
+
+
+            int height = Program.CalculateBinaryTreeHeight(root);
+
+            Assert.Equal(4, height);
+        }
     }
 }
