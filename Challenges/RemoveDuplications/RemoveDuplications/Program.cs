ce1967ac6eabc36823c3ab26832a88949984fa82
Deziree' Teague <dez.teague@gmail.com>
2019-02-11 19:36:16 -0800

    remove dupe implementation in progress

--- Challenges/RemoveDuplications/RemoveDuplications/Program.cs
+++ Challenges/RemoveDuplications/RemoveDuplications/Program.cs
@@ -8,13 +8,39 @@ namespace RemoveDuplications
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            Node node = new Node(1);
             LList list = new LList();
             list.Insert(1);
-            list.Insert(1);
             list.Insert(2);
+            list.Insert(2);
+            list.Insert(3);
+            list.Insert(3);
+
+            
         }
 
-        public static int RemoveDuplicates(LList list)
+        /// <summary>
+        /// remove duplicate nodes from a linked list
+        /// </summary>
+        /// <param name="node"></param>
+        //public static void RemoveDuplicates(Node node)
+        //{
+        //    var startingNode = node;
+           
+        //    while (startingNode != null)
+        //    {
+        //        var next = startingNode;
+        //        while(next.Next != null)
+        //        {
+        //            if (next.Next.Value.Equals(startingNode.Value))
+        //            {
+        //                next.Next = next.Next.Next;
+        //            }
+        //            else next = next.Next;
+        //        }
+        //        startingNode = startingNode.Next;
+        //    }
+           
+        //}
     }
 }
