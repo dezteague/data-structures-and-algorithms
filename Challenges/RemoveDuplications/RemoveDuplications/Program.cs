eba9f27ea084bb17dca8488f94d21cbe38a1cb23
Deziree' Teague <dez.teague@gmail.com>
2019-02-11 18:03:16 -0800

    instantiated list and inserted values

--- Challenges/RemoveDuplications/RemoveDuplications/Program.cs
+++ Challenges/RemoveDuplications/RemoveDuplications/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using LinkedList.Classes;
 
 namespace RemoveDuplications
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            LList list = new LList();
+            list.Insert(1);
+            list.Insert(1);
+            list.Insert(2);
         }
+
+        public static int RemoveDuplicates(LList list)
     }
 }
