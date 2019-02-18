e183be4ac859cda99af0d27f7a2ddeb17ed542d2
Deziree' Teague <dez.teague@gmail.com>
2019-02-17 20:45:00 -0800

    add method finished

--- Data-Structures/Hashtables/Hashtables/Classes/Hashtable.cs
+++ Data-Structures/Hashtables/Hashtables/Classes/Hashtable.cs
@@ -42,5 +42,37 @@ namespace Hashtables.Classes
             //return the index integer
             return indexValue;
         }
+
+        /// <summary>
+        /// Add a node to the table and set its key:value properties
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(string key, object value)
+        {
+            //instantiate a new node
+            Node newAddition = new Node(key, value);
+
+            //use the Hash method to get the key's hash index
+            int keyIndex = Hash(key);
+
+            //if there is no pre-existing value, assign it to the table
+            if(Htable[keyIndex]==null)
+            {
+                Htable[keyIndex] = newAddition;
+            }
+
+            //if there is a collision, create linked list
+            else
+            {
+                Node current = Htable[keyIndex];
+                while(current.Next != null)
+                {
+                    current = current.Next;
+                }
+                //new node is chained 
+                current.Next = newAddition;
+            }
+        }
     }
 }
