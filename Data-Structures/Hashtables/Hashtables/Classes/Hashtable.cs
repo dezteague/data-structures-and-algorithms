835fdd46d71936079b03b6bd94447a236b618fc3
Deziree' Teague <dez.teague@gmail.com>
2019-02-17 20:03:52 -0800

    added hash method

--- Data-Structures/Hashtables/Hashtables/Classes/Hashtable.cs
+++ Data-Structures/Hashtables/Hashtables/Classes/Hashtable.cs
@@ -6,14 +6,41 @@ namespace Hashtables.Classes
 {
     class Hashtable
     {
-        //Property: storage container for the nodes
+        //Properties: 
+        //storage container for the nodes
         public Node[] Htable { get; set; }
+        
+        //number of buckets
+        public int Buckets { get; set; }
 
         //constructor
         public Hashtable(int buckets)
         {
             //the number of buckets sets the length of the table "array"
             Htable = new Node[buckets];
+            Buckets = buckets;
+        }
+
+        /// <summary>
+        /// takes in an arbitrary key as a string, conducts the hash, and returns an index in the array
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>index</returns>
+        public int Hash(string key)
+        {
+            int indexValue = 0;
+
+            //add the ASCII value of each character
+            for (int i = 0; i < key.Length; i++)
+            {
+                indexValue += key[i];
+            }
+      
+            //divide the value by the number of buckets
+            indexValue = indexValue % Buckets;
+            
+            //return the index integer
+            return indexValue;
         }
     }
 }
