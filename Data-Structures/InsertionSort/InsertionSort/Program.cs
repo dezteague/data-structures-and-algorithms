3efdc1e1b3b65df388a619578d002dfd75fe2de9
Deziree' Teague <dez.teague@gmail.com>
2019-02-18 10:36:10 -0800

    insert sort method

---
+++ Data-Structures/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int [] InsertionSort(int [] array)
+        {
+            //go through the length of the input array
+            for (int i = 0; i < array.Length -1; i++)
+            {
+                //nested for loop uses j to compare the right adjacent index
+                for (int j = i + 1; j > 0; j--)
+                {
+                    //if the adjacent index to the left 
+                    if(array[j-1] > array[j])
+                    {
+                        int tempvalue = array[j - 1];
+
+                        //insert the greater value to the right
+                        array[j - 1] = array[j];
+                        array[j] = tempvalue;
+                    }
+                }
+            }
+            //return the sorted array
+            return array;
+        }
+    }
+}
