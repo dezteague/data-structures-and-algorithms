4b5751e6fe153f592de5f18a86380c38963a4872
Deziree' Teague <dez.teague@gmail.com>
2019-02-17 19:25:08 -0800

    set properties and constructor for Hashtable class

---
+++ Data-Structures/Hashtables/Hashtables/Classes/Hashtable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtables.Classes
+{
+    class Hashtable
+    {
+        //Property: storage container for the nodes
+        public Node[] Htable { get; set; }
+
+        //constructor
+        public Hashtable(int buckets)
+        {
+            //the number of buckets sets the length of the table "array"
+            Htable = new Node[buckets];
+        }
+    }
+}
