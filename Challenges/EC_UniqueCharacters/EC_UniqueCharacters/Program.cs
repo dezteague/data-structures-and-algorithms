3aaf2d8cc8b433f47587861ade041ff7ab14f0bb
Deziree' Teague <dez.teague@gmail.com>
2019-02-24 20:46:23 -0800

    inline comments

--- Challenges/EC_UniqueCharacters/EC_UniqueCharacters/Program.cs
+++ Challenges/EC_UniqueCharacters/EC_UniqueCharacters/Program.cs
@@ -17,16 +17,20 @@ namespace EC_UniqueCharacters
         /// <returns>true or false</returns>
         public bool Unique(string input)
         {
+            //instanitate a new hashtable
             Hashtable set = new Hashtable(1024);
 
             for(int i = 0; i < input.Length; i++ )
             {
                 string inputCharacter = Convert.ToString(input[i]);
             
+                //if the hashset already contains the character, it is a duplicate.  Therefore returns false
                 if(set.Contains(inputCharacter))
                 {
                     return false;
                 }
+
+                //if the hashset does not contain the character, add it.  
                 else
                 {
                     set.Add(inputCharacter, inputCharacter);
