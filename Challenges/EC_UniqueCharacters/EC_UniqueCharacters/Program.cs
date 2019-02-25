64e4224ce878d526f8c013c13f208dec960e7a22
Deziree' Teague <dez.teague@gmail.com>
2019-02-24 20:41:04 -0800

    set up if/else logic

---
+++ Challenges/EC_UniqueCharacters/EC_UniqueCharacters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Hashtables.Classes;
+
+namespace EC_UniqueCharacters
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool Unique(string input)
+        {
+            Hashtable set = new Hashtable(1024);
+
+            for(int i = 0; i < input.Length; i++ )
+            {
+            
+                if(set.Contains(input[i]))
+                {
+                    return false;
+                }
+                else
+                {
+                    set.Add(input[i]);
+                }
+            }
+        }
+    }
+}
